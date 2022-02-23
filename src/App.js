819e8ab47170f8a9a99ddbc1afc3e585f0f2a55a
Tony <jicstar94@gmail.com>
2022-02-23 15:13:05 +0100

    add css and form for main app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.scss';
+import { useState } from 'react';
 import WeatherWidget from './components/WeatherWidget';
 
 function App() {
+  const [city, setCity] = useState();
+  const [value, setValue] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setCity(value);
+  };
  
   return (
     <div className="app">
-      
+      <form onSubmit={handleSubmit} className="form">
+        <input
+          className="form__input"
+          type="text"
+          placeholder="Saisir la ville"
+          value={value}
+          onChange={(event) => setValue(event.target.value)}
+        />
+      </form>
+      <WeatherWidget />
     </div>
   );
 }
--- src/components/WeatherWidget/index.js
+++ src/components/WeatherWidget/index.js
@@ -1,10 +1,14 @@
 import './style.scss';
 
-export default function WeatherWidget() {
 
+export default function WeatherWidget() {
   return (
     <div className="weather-widget">
-      
+      <div className="weather-widget__infos">
+        <p className="weather-widget__city">Paris</p>
+        <p className="weather-widget__desc">desc</p>
+      </div>
+      <div className="weather-widget__temperature">5<sup>°C</sup></div>
     </div>
   );
 }
